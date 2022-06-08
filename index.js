feb70f425e4c85e964f87d6de839d3ac41246e23
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-08 09:30:23 +0300

    add callback functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const receivesAFunction = (callback) => {
+  callback();
+};
+const namedFunction = () => {};
+const returnsANamedFunction = () => {
+  return namedFunction;
+};
+const anonymousFunction = () => {};
+const returnsAnAnonymousFunction = () => {
+  return () => {};
+};
+
